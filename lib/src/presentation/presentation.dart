db3b78fb665144da2efaeec11e0afa68ceb70c44
Tomáš Novák <hello@tom-novak.com>
2022-01-31 17:29:23 +0100

    Support for displaying book images

--- lib/src/presentation/presentation.dart
+++ lib/src/presentation/presentation.dart
@@ -1,4 +1,5 @@
 export 'book_detail_screen.dart';
 export 'book_list_screen.dart';
+export 'constants.dart';
 export 'utils/book_extension.dart';
 export 'widgets/export.dart';
--- lib/src/presentation/utils/book_extension.dart
+++ lib/src/presentation/utils/book_extension.dart
@@ -1,4 +1,5 @@
 import 'package:bookstore_demo/src/infrastructure/api/model/book.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_common_widgets/flutter_common_widgets.dart';
 
 extension BookX on Book {
@@ -6,6 +7,7 @@ extension BookX on Book {
     return CommonItem(
       title: title,
       subtitle: subtitle,
+      image: image != null ? CachedNetworkImageProvider(image!) : null,
     );
   }
 }
