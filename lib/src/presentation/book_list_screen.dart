f155bea7b8c93796d04f7bfb4032a79d4e5ec578
Tomáš Novák <hello@tom-novak.com>
2022-02-15 14:20:49 +0100

    Dont hide search input on soft keyboard

--- lib/src/presentation/book_list_screen.dart
+++ lib/src/presentation/book_list_screen.dart
@@ -47,83 +47,90 @@ class _BookListScreenState extends State<BookListScreen> {
       appBar: AppBar(
         title: const Text('Book search'),
       ),
-      body: RefreshIndicator(
-        onRefresh: () async {
-          context.read<BookListCubit>().refresh();
-          await context.read<BookListCubit>().stream.firstWhere((state) {
-            return state.isLoading == false;
-          });
-        },
-        child: BlocBuilder<BookListCubit, BookListState>(
-          builder: (context, state) {
-            return state.failureOrSuccessOption.fold(
-              // without data
-              () {
-                return state.isLoading
-                    ? const CommonListShimmer(itemCount: 7)
-                    : const BookListInitContent();
+      body: Column(
+        children: [
+          Expanded(
+            child: RefreshIndicator(
+              onRefresh: () async {
+                context.read<BookListCubit>().refresh();
+                await context.read<BookListCubit>().stream.firstWhere((state) {
+                  return state.isLoading == false;
+                });
               },
-              (valueOrFailure) => valueOrFailure.fold(
-                (dataFailure) => CommonErrorPage(
-                    label: CommonLocalizations.of(context)!.error,
-                    description:
-                        CommonLocalizations.of(context)!.somethingWrong),
-                (data) {
-                  return SliverListPage(
-                    controller: _controller,
-                    itemBuilder: (context, index) {
-                      var item = data.books[index];
-                      return CommonListTile(
-                        item: item.toCommonItem(),
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return BlocProvider(
-                                  create: (context) => BookDetailCubit(
-                                      repository: getIt.get<IBookRepository>()),
-                                  child: BookDetailScreen(
-                                    bookPreview: item,
+              child: BlocBuilder<BookListCubit, BookListState>(
+                builder: (context, state) {
+                  return state.failureOrSuccessOption.fold(
+                    // without data
+                    () {
+                      return state.isLoading
+                          ? const CommonListShimmer(itemCount: 7)
+                          : const BookListInitContent();
+                    },
+                    (valueOrFailure) => valueOrFailure.fold(
+                      (dataFailure) => CommonErrorPage(
+                          label: CommonLocalizations.of(context)!.error,
+                          description:
+                              CommonLocalizations.of(context)!.somethingWrong),
+                      (data) {
+                        return SliverListPage(
+                          controller: _controller,
+                          itemBuilder: (context, index) {
+                            var item = data.books[index];
+                            return CommonListTile(
+                              item: item.toCommonItem(),
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return BlocProvider(
+                                        create: (context) => BookDetailCubit(
+                                            repository:
+                                                getIt.get<IBookRepository>()),
+                                        child: BookDetailScreen(
+                                          bookPreview: item,
+                                        ),
+                                      );
+                                    },
                                   ),
                                 );
                               },
-                            ),
-                          );
-                        },
-                      );
-                    },
-                    separatorBuilder: (context, index) => const Divider(),
-                    itemCount: data.books.length,
-                    layoutStateBuilder: (context) {
-                      return LayoutState.content;
-                    },
-                    footer: LoadingIndicator(
-                      status: state.isLoading
-                          ? LoadingStatus.loading
-                          : LoadingStatus.idle,
+                            );
+                          },
+                          separatorBuilder: (context, index) => const Divider(),
+                          itemCount: data.books.length,
+                          layoutStateBuilder: (context) {
+                            return LayoutState.content;
+                          },
+                          footer: LoadingIndicator(
+                            status: state.isLoading
+                                ? LoadingStatus.loading
+                                : LoadingStatus.idle,
+                          ),
+                        );
+                      },
                     ),
                   );
                 },
               ),
-            );
-          },
-        ),
-      ),
-      bottomNavigationBar: Material(
-        elevation: 20,
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: SearchForm(
-                onSubmit: (value) {
-                  context.read<BookListCubit>().search(SearchQuery(value));
-                },
+            ),
+          ),
+          Material(
+            elevation: 20,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: SearchForm(
+                    onSubmit: (value) {
+                      context.read<BookListCubit>().search(SearchQuery(value));
+                    },
+                  ),
+                ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
